61c94048ef63e5581816fdc5c708fc082f5ede30
nmerkel <nmerkel@umd.edu>
2020-04-08 14:50:25 -0400

    updating tweets.py

--- twitter_scraper/modules/tweets.py
+++ twitter_scraper/modules/tweets.py
@@ -4,23 +4,24 @@ from datetime import datetime
 from urllib.parse import quote
 from lxml.etree import ParserError
 import mechanicalsoup
+import xlsxwriter # for writing to excel wkbk
 
 session = HTMLSession()
+
 
 browser = mechanicalsoup.StatefulBrowser()
 browser.addheaders = [('User-agent', 'Firefox')]
 
-def get_tweets(query, pages=25):
+def get_tweets(query, party, numTweets = 500):
     """Gets tweets for a given user, via the Twitter frontend API."""
 
     after_part = f'include_available_features=1&include_entities=1&include_new_items_bar=true'
-    if query.startswith('#'):
-        query = quote(query)
-        url = f'https://twitter.com/i/search/timeline?f=tweets&vertical=default&q={query}&src=tyah&reset_error_state=false&'
-    else:
-        url = f'https://twitter.com/i/profiles/show/{query}/timeline/tweets?'
+    url = f'https://twitter.com/i/profiles/show/{query}/timeline/tweets?'
     url += after_part
-    
+    workbook = xlsxwriter.Workbook('scrubbed_tweets/' + query + '.xlsx') # into albert's data folder
+    worksheet = workbook.add_worksheet()
+
+
     headers = {
         'Accept': 'application/json, text/javascript, */*; q=0.01',
         'Referer': f'https://twitter.com/{query}',
@@ -30,8 +31,10 @@ def get_tweets(query, pages=25):
         'Accept-Language': 'en-US'
     }
 
-    def gen_tweets(pages):
+    def gen_tweets(party,numTweets):
         r = session.get(url, headers=headers)
+        row = 0
+        pages = 1
 
         while pages > 0:
             try:
@@ -53,85 +56,35 @@ def get_tweets(query, pages=25):
                     text = tweet.find('.tweet-text')[0].full_text
                 except IndexError:  # issue #50
                     continue
+                tweets.append(text)
 
-                tweet_id = tweet.attrs['data-item-id']
-
-                time = datetime.fromtimestamp(int(tweet.find('._timestamp')[0].attrs['data-time-ms']) / 1000.0)
-
-                interactions = [
-                    x.text
-                    for x in tweet.find('.ProfileTweet-actionCount')
-                ]
-
-                replies = int(
-                    interactions[0].split(' ')[0].replace(comma, '').replace(dot, '')
-                    or interactions[3]
-                )
-
-                retweets = int(
-                    interactions[1].split(' ')[0].replace(comma, '').replace(dot, '')
-                    or interactions[4]
-                    or interactions[5]
-                )
-
-                likes = int(
-                    interactions[2].split(' ')[0].replace(comma, '').replace(dot, '')
-                    or interactions[6]
-                    or interactions[7]
-                )
-
-                hashtags = [
-                    hashtag_node.full_text
-                    for hashtag_node in tweet.find('.twitter-hashtag')
-                ]
-                urls = [
-                    url_node.attrs['data-expanded-url']
-                    for url_node in tweet.find('a.twitter-timeline-link:not(.u-hidden)')
-                ]
-                photos = [
-                    photo_node.attrs['data-image-url']
-                    for photo_node in tweet.find('.AdaptiveMedia-photoContainer')
-                ]
-
-                is_retweet = True if tweet.find('.js-stream-tweet')[0].attrs.get('data-retweet-id', None) \
-                    else False
-
-                videos = []
-                video_nodes = tweet.find(".PlayableMedia-player")
-                for node in video_nodes:
-                    styles = node.attrs['style'].split()
-                    for style in styles:
-                        if style.startswith('background'):
-                            tmp = style.split('/')[-1]
-                            video_id = tmp[:tmp.index('.jpg')] if '.jpg' in tmp \
-                                else tmp[:tmp.index('.png')] if '.png' in tmp else None
-                            videos.append({'id': video_id})
-
-                tweets.append({
-                    'tweetId': tweet_id,
-                    'isRetweet': is_retweet,
-                    'time': time,
-                    'text': text,
-                    'replies': replies,
-                    'retweets': retweets,
-                    'likes': likes,
-                    'entries': {
-                        'hashtags': hashtags, 'urls': urls,
-                        'photos': photos, 'videos': videos
-                    }
-                })
 
             last_tweet = html.find('.stream-item')[-1].attrs['data-item-id']
 
             for tweet in tweets:
-                    tweet['text'] = re.sub(r'(\S)http', '\g<1> http', tweet['text'], 1)
-                    tweet['text'] = re.sub(r'(\S)pic\.twitter', '\g<1> pic.twitter', tweet['text'], 1)
+                    uncleaned = tweet
+                    tweet = re.sub(r'https?:\/\/\S+', '', tweet) # remove links
+                    tweet = re.sub(r'pic\.twitter\S+', '', tweet) # remove pictures
+                    # if no text left... delete that tweet! punctuation?
+                    stripped = tweet.strip()
+                    if stripped == '':
+                        tweets.remove(uncleaned)
+                        continue
+                    worksheet.write(row, 0, tweet)
+                    worksheet.write(row, 1, party)
+                    row += 1
+                    if row == numTweets:
+                        pages = 0
+                        break
                     yield tweet
 
-            r = session.get(url, params={'max_position': last_tweet}, headers=headers)
-            pages += -1
 
-    yield from gen_tweets(pages)
+            r = session.get(url, params={'max_position': last_tweet}, headers=headers)
+            pages += 1
+
+    yield from gen_tweets(party, numTweets)
+    workbook.close()
+
 
 # for searching:
 #
